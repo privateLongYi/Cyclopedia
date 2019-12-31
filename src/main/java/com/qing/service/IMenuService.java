2dc3b635f05b85aec6eff76dfcb40403861ead57
ly_glb <1329645742@qq.com>
2019-12-31 16:11:24 +0800

    龙意

---
+++ src/main/java/com/qing/service/IMenuService.java
@@ -0,0 +1,13 @@
+package com.qing.service;
+
+import com.qing.entity.Menu;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IMenuService {
+
+    //查询所有
+    List<Menu> getMenu();
+
+}
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,40 @@
+$(function () {
+    $.post("getMenu", function (data) {
+
+        var html = "<div class='panel panel-default'>" +
+                    "     <div class='panel-heading'>";
+
+        //遍历数据
+        for (var i = 0; i < data.length; i++){
+
+            html += "   <a class='open-menu green-left' href=#collapse" + i +
+                    "       data-toggle='collapse' data-parent='#accordion'>" +
+                    "       <spann th:class='glyphicon glyphicon-cog'></span>" +
+                    "       <span>" + data[i].name + "</span>" +
+                    "       <span class='glyphicon glyphicon-menu-left pull-right'></span>" +
+                    "   </a>" +
+                    "</div>" +
+                    "<div id=collapse"+i+" class='panel-collapse collapse'>" +
+                    "   <ul>";
+
+            if (data[i].subset.length > 0) {
+                for (var j = 0; j < data[i].subset.length; j++) {
+                    html += "<li>" +
+                            "   <a href='" + data[i].subset[j].url + "'>" +
+                            "       <span class='glyphicon glyphicon-record'></span>" +
+                            "       <span>" + data[i].subset[j].name + "</span>" +
+                            "   </a>" +
+                            "</li>";
+                }
+            }
+
+            html += "          </ul>" +
+                    "     </div>" +
+                    "</div>";
+
+        }
+
+        $("#accordion").html(html);
+
+    });
+})
